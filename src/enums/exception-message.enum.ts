d88107dbba033844c92aa25b473c904cbbb87726
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-10-21 16:34:35 -0300

    feat: add exception messages

---
+++ src/enums/exception-message.enum.ts
@@ -0,0 +1,7 @@
+export enum EXCEPTION_MESSAGE {
+  EMAIL_EXISTS = 'Email already registered.',
+  USER_NOT_FOUND = 'User not found.',
+  WRONG_CREDENTIALS = 'Email or password wrong.',
+  USER_UPDATE_NOT_ALLOWED = 'You are not allowed to update this user.',
+  DELETE_USER_NOT_ALLOWED = 'You are not allowed to delete this user.',
+}
