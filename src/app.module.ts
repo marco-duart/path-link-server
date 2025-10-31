25bbcc8d16979279d2e18a69176f8af72c5b9f72
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-10-31 13:23:08 -0300

    feat: update module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,40 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { AssetsModule } from './assets/assets.module';
+import { UsersModule } from './users/users.module';
+import { ConfigurationItemsModule } from './configuration-items/configuration-items.module';
+import { DatabasesModule } from './databases/databases.module';
+import { DepartmentsModule } from './departments/departments.module';
+import { EnvironmentVariablesModule } from './environment-variables/environment-variables.module';
+import { LinksModule } from './links/links.module';
+import { TeamsModule } from './teams/teams.module';
+import { RepositoriesModule } from './repositories/repositories.module';
+import { StepAssetsModule } from './step-assets/step-assets.module';
+import { StepsModule } from './steps/steps.module';
+import { ProcessesModule } from './processes/processes.module';
+import { StepRelationshipsModule } from './step-relationships/step-relationships.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    AccountsModule,
+    AssetsModule,
+    UsersModule,
+    ConfigurationItemsModule,
+    DatabasesModule,
+    DepartmentsModule,
+    EnvironmentVariablesModule,
+    LinksModule,
+    TeamsModule,
+    RepositoriesModule,
+    StepAssetsModule,
+    StepsModule,
+    ProcessesModule,
+    StepRelationshipsModule,
+  ],
 })
 export class AppModule {}
