0d3f2395eca738776177f915c5d02b435aef9052
Marco Aurélio <marcoaurelio_db@hotmail.com>
2026-02-09 17:35:01 -0300

    feat: update account module

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -3,9 +3,10 @@ import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from '../database/entities/account.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account]), AuthModule],
   controllers: [AccountsController],
   providers: [AccountsService],
   exports: [AccountsService],
