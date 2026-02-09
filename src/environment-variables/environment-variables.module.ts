36122f32261a1e5b159fdded263dfa733613ab82
Marco Aurélio <marcoaurelio_db@hotmail.com>
2026-02-09 17:37:00 -0300

    feat: update env module

--- src/environment-variables/environment-variables.module.ts
+++ src/environment-variables/environment-variables.module.ts
@@ -3,9 +3,10 @@ import { EnvironmentVariablesService } from './environment-variables.service';
 import { EnvironmentVariablesController } from './environment-variables.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnvironmentVariable } from '../database/entities/environment-variable.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EnvironmentVariable])],
+  imports: [TypeOrmModule.forFeature([EnvironmentVariable]), AuthModule],
   controllers: [EnvironmentVariablesController],
   providers: [EnvironmentVariablesService],
   exports: [EnvironmentVariablesService],
