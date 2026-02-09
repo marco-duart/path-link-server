f741c40ba9b6cd9d5c0f52ab98f3cb5e2ff2c4f2
Marco Aurélio <marcoaurelio_db@hotmail.com>
2026-02-09 17:36:50 -0300

    feat: update department module

--- src/departments/departments.module.ts
+++ src/departments/departments.module.ts
@@ -3,9 +3,10 @@ import { DepartmentsService } from './departments.service';
 import { DepartmentsController } from './departments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Department } from '../database/entities/department.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Department])],
+  imports: [TypeOrmModule.forFeature([Department]), AuthModule],
   controllers: [DepartmentsController],
   providers: [DepartmentsService],
   exports: [DepartmentsService],
