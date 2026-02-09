65a65cdbb61c70b6a116b2a3fc77baea90a78c1f
Marco Aurélio <marcoaurelio_db@hotmail.com>
2026-02-09 17:37:17 -0300

    feat: update processes module

--- src/processes/processes.module.ts
+++ src/processes/processes.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ProcessesService } from './processes.service';
 import { ProcessesController } from './processes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Process } from '../database/entities/process.entity';
 import { UsersModule } from '../users/users.module';
 import { StepsModule } from '../steps/steps.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Process]), UsersModule, StepsModule],
+  imports: [TypeOrmModule.forFeature([Process]), UsersModule, forwardRef(() => StepsModule), AuthModule],
   controllers: [ProcessesController],
   providers: [ProcessesService],
   exports: [ProcessesService],
