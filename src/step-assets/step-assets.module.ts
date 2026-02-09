28cb65e38855d58ccd8893cc20a2587bae7e493f
Marco Aurélio <marcoaurelio_db@hotmail.com>
2026-02-09 17:37:27 -0300

    feat: update step asset module

--- src/step-assets/step-assets.module.ts
+++ src/step-assets/step-assets.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { StepAssetsService } from './step-assets.service';
 import { StepAssetsController } from './step-assets.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StepAsset } from '../database/entities/step-asset.entity';
 import { AssetsModule } from '../assets/assets.module';
 import { StepsModule } from '../steps/steps.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StepAsset]), AssetsModule, StepsModule],
+  imports: [TypeOrmModule.forFeature([StepAsset]), AssetsModule, forwardRef(() => StepsModule), AuthModule],
   controllers: [StepAssetsController],
   providers: [StepAssetsService],
   exports: [StepAssetsService],
