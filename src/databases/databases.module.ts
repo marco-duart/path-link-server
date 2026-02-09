a696d0e4261ed1522f4beca566c0744aa9b7d91b
Marco Aurélio <marcoaurelio_db@hotmail.com>
2026-02-09 17:36:41 -0300

    feat: update database module

--- src/databases/databases.module.ts
+++ src/databases/databases.module.ts
@@ -3,9 +3,10 @@ import { DatabasesService } from './databases.service';
 import { DatabasesController } from './databases.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Database } from '../database/entities/database.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Database])],
+  imports: [TypeOrmModule.forFeature([Database]), AuthModule],
   controllers: [DatabasesController],
   providers: [DatabasesService],
   exports: [DatabasesService],
