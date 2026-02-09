8d3d63955c73e742eb317aa7af2aab924c3c60b3
Marco Aurélio <marcoaurelio_db@hotmail.com>
2026-02-09 17:37:08 -0300

    feat: update repo module

--- src/repositories/repositories.module.ts
+++ src/repositories/repositories.module.ts
@@ -3,9 +3,10 @@ import { RepositoriesService } from './repositories.service';
 import { RepositoriesController } from './repositories.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Repository } from '../database/entities/repository.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Repository])],
+  imports: [TypeOrmModule.forFeature([Repository]), JwtModule],
   controllers: [RepositoriesController],
   providers: [RepositoriesService],
   exports: [RepositoriesService],
