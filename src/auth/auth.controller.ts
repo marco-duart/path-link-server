4d457fd656c6db8d1437d96d6ab414e91b227641
Marco Aurélio <marcoaurelio_db@hotmail.com>
2026-02-10 17:25:58 -0300

    feat: update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { LoginDto } from './dto/login.dto';
 import { CurrentUser } from '../decorators/current-user.decorator';
 import { CurrentUserDto } from '../decorators/dto/current-user.dto';
 import { AuthGuard } from './guards/auth.guard';
+import { AuthResponseDto, UserAuthDto } from './dto/auth-response.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -16,13 +17,13 @@ export class AuthController {
   }
 
   @Post('login')
-  login(@Body() loginDto: LoginDto) {
+  login(@Body() loginDto: LoginDto): Promise<AuthResponseDto> {
     return this.authService.login(loginDto);
   }
 
   @UseGuards(AuthGuard)
   @Get('me')
-  me(@CurrentUser() user: CurrentUserDto) {
+  me(@CurrentUser() user: CurrentUserDto): Promise<UserAuthDto> {
     return this.authService.me(user.user);
   }
 }
