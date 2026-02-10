de36db7f228b3a067aaef929cee7952c7544df68
Marco Aurélio <marcoaurelio_db@hotmail.com>
2026-02-10 17:26:03 -0300

    feat: update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConfig } from './jwt/jwt.config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [forwardRef(() => UsersModule), JwtModule.registerAsync(jwtConfig)],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [JwtModule],
 })
 export class AuthModule {}
