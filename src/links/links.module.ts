3aada05bbf0da07d5a02e823738f49505edeb755
Marco Aurélio <marcoaurelio_db@hotmail.com>
2026-02-10 17:26:44 -0300

    feat: update

--- src/links/links.module.ts
+++ src/links/links.module.ts
@@ -3,9 +3,10 @@ import { LinksService } from './links.service';
 import { LinksController } from './links.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Link } from '../database/entities/link.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Link])],
+  imports: [TypeOrmModule.forFeature([Link]), AuthModule],
   controllers: [LinksController],
   providers: [LinksService],
   exports: [LinksService],
